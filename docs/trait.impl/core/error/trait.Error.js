d9e21c33e6b9d064e34915127d3a47e29dce828f
astinz <astinz@users.noreply.github.com>
2025-12-23 07:24:40 +0000

    deploy: fad7296acc74a3a6ba58f698b06859aa18393a03

---
+++ docs/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["fastcrypto",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"fastcrypto/error/enum.FastCryptoError.html\" title=\"enum fastcrypto::error::FastCryptoError\">FastCryptoError</a>"]]],["fastkrypto_uniffi",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"fastkrypto_uniffi/enum.FastCryptoFfiError.html\" title=\"enum fastkrypto_uniffi::FastCryptoFfiError\">FastCryptoFfiError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[303,321]}
---
+++ docs/trait.impl/uniffi_core/ffi_converter_traits/trait.Lower.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["fastkrypto_uniffi",[["impl&lt;UT&gt; Lower&lt;UT&gt; for <a class=\"enum\" href=\"fastkrypto_uniffi/enum.FastCryptoFfiError.html\" title=\"enum fastkrypto_uniffi::FastCryptoFfiError\">FastCryptoFfiError</a>"],["impl&lt;UT&gt; Lower&lt;UT&gt; for <a class=\"enum\" href=\"fastkrypto_uniffi/enum.SignatureScheme.html\" title=\"enum fastkrypto_uniffi::SignatureScheme\">SignatureScheme</a>"],["impl&lt;UT&gt; Lower&lt;UT&gt; for <a class=\"struct\" href=\"fastkrypto_uniffi/struct.KeyPairBytes.html\" title=\"struct fastkrypto_uniffi::KeyPairBytes\">KeyPairBytes</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[569]}
